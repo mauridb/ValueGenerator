0b3b2fab505fc4f88b25c92a33fbe6fe837e8cb0
mauridb <mauriziocontatto@gmail.com>
2017-11-23 12:04:32 +0100

    completed script interval to generate fakes value

--- generator_values.py
+++ generator_values.py
@@ -1,14 +1,12 @@
+from datetime import datetime, timedelta
 import csv
-import datetime
 import logging
 import random
+import sys
 
 logging.basicConfig(filename='data.log', level=logging.DEBUG)
 
-
-# TODO: time +15min, id device and id pom
-
-
+_author_ = 'Maurizio Bussi'
 
 # static variable
 MIN_VALUE = 3.0
@@ -18,6 +16,8 @@ AVAILABLE_OPTIONS = [
 	round(random.uniform(MIN_VALUE, MAX_VALUE), 1),
 	round(random.uniform(MIN_VALUE, MIN_VALUE - THRES_TOLL), 1),
 	round(random.uniform(MAX_VALUE, MAX_VALUE + THRES_TOLL), 1),
+	round(random.uniform(0.0, MIN_VALUE - THRES_TOLL - 0.1), 1),
+	round(random.uniform(MAX_VALUE + THRES_TOLL + 0.1, 8.0), 1),
 	'null'
 ]
 # print AVAILABLE_OPTIONS[1]
@@ -25,6 +25,16 @@ AVAILABLE_OPTIONS = [
  
 
 dates_to_check = ['20/11/2017', '21/11/2017', '22/11/2017']
+
+def datetime_range(start, end, delta):
+    current = start
+    while current < end:
+        yield current
+        current += delta
+
+dts = [dt.strftime('%H:%M') for dt in 
+       datetime_range(datetime(2017, 11, 20, 0), datetime(2017, 11, 21, 0), 
+       timedelta(minutes=15))]
 
 def write_on_file():
 	logging.info('start writing the file')
@@ -37,9 +47,9 @@ def write_on_file():
 			for i in range(0, 96):
 				writer.writerow({
 					'DATA': dt,
-					'ORA': 'ancora da sistemare',
-					'POM_ID': 'da sistemare' ,
-					'DEVICE_ID': 'da sistemare',
+					'ORA': dts[i],
+					'POM_ID': sys.argv[1],
+					'DEVICE_ID': sys.argv[1],
 					'VALUE': AVAILABLE_OPTIONS[random.randint(0, len(AVAILABLE_OPTIONS) - 1)],
 				})
 
@@ -49,4 +59,5 @@ def start():
 	logging.info("file generated successfully!!\n-------------------------------")
 
 
-start()      
+start()
+
